502d53f41cea1cad433046e87d80c0005f3db6ef
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-12 03:19:42 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
-def my_collect(languages)
+def my_collect(collection)
   i = 0
   collection = []
   while i < array.length
-    collection << yield(languages[i])
+    collection << yield(students[i])
     i += 1
   end
   collection
